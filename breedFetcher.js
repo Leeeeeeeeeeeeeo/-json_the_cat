adaa4ed21461ebf36b6eaa250b7ffd52831d03c1
Your Name <akas3524@yahoo.com>
2020-09-24 20:45:21 +0000

    cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,24 @@
+
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}` , (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
 
-});
- 
+const fetchBreedDescription = function(breedName, callback) {
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}` , (error, response, body) => {
+  if(error){
+    callback(error)
+  }
+  const data = JSON.parse(body);  
+  const description = data[0].description
+
+  if(!error) callback(null,description);
+  
+  });
+}  
+
+module.exports = { fetchBreedDescription };
 
 
+
+// fetchBreedDescription('Siberian', (error, description) {
+
+// });
